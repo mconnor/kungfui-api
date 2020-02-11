bbf8e3b8b0c505187be7f9d2106afedd11ae6498
mike connor <mike@rocketnumber9.org>
2020-02-11 15:30:06 -0600

    001 resolvers and queries

---
+++ index.js
@@ -0,0 +1,35 @@
+const {
+    ApolloServer,
+    gql
+} = require("apollo-server");
+
+const typeDefs = gql `
+    type Movie {
+        title: String
+        releaseDate: String
+        rating: Int
+    }
+
+    type Query {
+        movies: [Movie]
+    }
+`;
+
+const movies =[
+    {
+    title: "5 Deadly Venoms",
+    releaseDate: "10-10-1983",
+    rating: 5
+  },
+  {
+    title: "36th Chamber",
+    releaseDate: "10-10-1983",
+    rating: 5
+  }
+];
+
+const resolvers = {
+    Query: {
+        movies: () => movies;
+    }
+}
