d5b119f1cf5b75e3c372193a1b5e1911c382b8df
mike connor <mike@rocketnumber9.org>
2020-02-11 17:05:53 -0600

    static api

--- index.js
+++ index.js
@@ -4,32 +4,44 @@ const {
 } = require("apollo-server");
 
 const typeDefs = gql `
-    type Movie {
-        title: String
-        releaseDate: String
-        rating: Int
-    }
+  type Movie {
+    title: String
+    releaseDate: String
+    rating: Int
+  }
 
-    type Query {
-        movies: [Movie]
-    }
+  type Query {
+    movies: [Movie]
+  }
 `;
 
-const movies =[
+const movies = [{
+        title: "5 Deadly Venoms",
+        releaseDate: "10-10-1983",
+        rating: 5
+    },
     {
-    title: "5 Deadly Venoms",
-    releaseDate: "10-10-1983",
-    rating: 5
-  },
-  {
-    title: "36th Chamber",
-    releaseDate: "10-10-1983",
-    rating: 5
-  }
+        title: "36th Chamber",
+        releaseDate: "10-10-1983",
+        rating: 5
+    }
 ];
 
 const resolvers = {
     Query: {
-        movies: () => movies;
+        movies: () => {
+            return movies;
+        }
     }
-}
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen().then(({
+    url
+}) => {
+    console.log(`Server started at ${url}`);
+});
