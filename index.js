4d8aab8d9e912405b302d1bd9c3d7e00a5b39955
mike connor <mike@rocketnumber9.org>
2020-02-12 17:15:30 -0600

    custom resolvers/nested relations

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const {
     ApolloServer,
     gql
 } = require("apollo-server");
-const { GraphQLScalarType } = require('graphql');
-const { Kind } = require('graphql/language')
+const {
+    GraphQLScalarType
+} = require('graphql');
+const {
+    Kind
+} = require('graphql/language')
 
 const typeDefs = gql `
     scalar Date
@@ -25,8 +29,8 @@ const typeDefs = gql `
         title: String!
         releaseDate: String
         rating: Int
-        actor: [Actor]
-        # status: Status
+        actors: [Actor]
+        status: Status
     }
 
     type Query {
@@ -34,21 +38,32 @@ const typeDefs = gql `
         movie(id: ID): Movie
     }
 `;
+//   end of graphql
+const actors = [{
+        id: "gordon",
+        name: "Gordon Liu"
+    },
+    {
+        id: "jackie",
+        name: "Jackie Chan"
+    }
+];
 
 const movies = [{
         id: 'dkdfk',
         title: "5 Deadly Venoms",
         releaseDate: "10-10-1983",
-        rating: 5
+        actors: [{
+            id: "jackie"
+        }]
     },
     {
         id: 'xsssksks',
         title: "36th Chamber",
-        releaseDate: "10-10-1983",
+        releaseDate: "10-07-1983",
         rating: 5,
-        actor: [{
-            id: "asdfasdf",
-            name: "Gordon Liu"
+        actors: [{
+            id: "gordon"
         }]
     }
 ];
@@ -68,6 +83,20 @@ const resolvers = {
             return foundMovie;
         },
     },
+
+    Movie: {
+        actors: (obj, arg, context) => {
+            // DB Call
+            const actorIds = obj.actors.map(actor => actor.id);
+            const filteredActors = actors.filter(actor => {
+                return actorIds.includes(actor.id);
+            });
+            return filteredActors;
+
+            // return actors.filter(actor => actorIds.includes(actor.id));
+        }
+    },
+
     Date: new GraphQLScalarType({
         name: 'Date',
         description: "it's a date, deal",
@@ -77,7 +106,7 @@ const resolvers = {
         serialize(value) {
             return value
         },
-        parseLiteral(ast){
+        parseLiteral(ast) {
             if (ast.kind === Kind.INT) {
                 return new Date(ast.value)
             }
