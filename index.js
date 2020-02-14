c3a9bf29abcba8a247ee02b53c1c9c0a30a4db24
mike connor <mike@rocketnumber9.org>
2020-02-14 16:32:57 -0600

    pull out unneded set up code

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const {
     Kind
 } = require('graphql/language');
 
+//for using .env file
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -24,9 +25,9 @@ mongoose.connect(
 
 const db = mongoose.connection;
 
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-var movieSchema = new Schema({
+const movieSchema = new Schema({
     title: String,
     releaseDate: Date,
     rating: Number,
@@ -90,34 +91,6 @@ const typeDefs = gql `
     
 `;
 //   end of graphql
-const actors = [{
-        id: "gordon",
-        name: "Gordon Liu"
-    },
-    {
-        id: "jackie",
-        name: "Jackie Chan"
-    }
-];
-
-const movies = [{
-        id: 'dkdfk',
-        title: "5 Deadly Venoms",
-        releaseDate: "10-10-1983",
-        actors: [{
-            id: "jackie"
-        }]
-    },
-    {
-        id: 'xsssksks',
-        title: "36th Chamber",
-        releaseDate: "10-07-1983",
-        rating: 5,
-        actors: [{
-            id: "gordon"
-        }]
-    }
-];
 
 const pubsub = new PubSub();
 const MOVIE_ADDED = 'MOVIE_ADDED'
@@ -149,19 +122,6 @@ const resolvers = {
             }
         },
     },
-
-    // Movie: {
-    //     actors: (obj, arg, context) => {
-    //         // DB Call
-    //         const actorIds = obj.actors.map(actor => actor.id);
-    //         const filteredActors = actors.filter(actor => {
-    //             return actorIds.includes(actor.id);
-    //         });
-    //         return filteredActors;
-
-    //         // return actors.filter(actor => actorIds.includes(actor.id));
-    //     }
-    // },
 
     Mutation: {
        addMovie:  async (obj, { movie }, { userId }) => {
